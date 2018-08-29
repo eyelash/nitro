63621d2156b999c00157bda26a377d69bcb3fe4b
Elias Aebi <eyelash@users.noreply.github.com>
2018-08-29 18:24:06 +0200

    remove the widgets

--- demo.cpp
+++ demo.cpp
@@ -2,47 +2,8 @@
 
 using namespace nitro;
 
-class DemoWidget: public BlurredRectangle {
-	RoundedRectangle rounded_rectangle;
-	RoundedBorder border;
-	Alignment alignment;
-	Text text;
-	Animator<Color> animator;
-public:
-	DemoWidget(FontSet* font):
-		BlurredRectangle(Color::create(0, 0, 0, 0.2), 8, 20),
-		rounded_rectangle(Color::create(0.2, 0.9, 0.7), 10),
-		border(2, Color::create(0, 0, 0, 0.2), 10),
-		alignment(HorizontalAlignment::CENTER, VerticalAlignment::CENTER),
-		text(font, "some text", Color::create(0, 0, 0, 0.5)),
-		animator(rounded_rectangle.color())
-	{
-		set_child(&rounded_rectangle);
-		rounded_rectangle.set_child(&border);
-		border.set_child(&alignment);
-		alignment.set_child(&text);
-	}
-	void mouse_enter() override {
-		BlurredRectangle::mouse_enter();
-		animator.animate(Color::create(0.5, 0.9, 0.4), 200);
-	}
-	void mouse_leave() override {
-		BlurredRectangle::mouse_leave();
-		animator.animate(Color::create(0.2, 0.9, 0.7), 200);
-	}
-};
-
 int main() {
 	Window window(800, 500, "demo");
-	FontSet font("Roboto", 16);
-
-	Rectangle background(Color::create(0.9, 0.9, 0.9));
-	window.set_child(&background);
-	Padding padding(100);
-	background.set_child(&padding);
-
-	DemoWidget widget(&font);
-	padding.set_child(&widget);
 
 	window.run();
 }
